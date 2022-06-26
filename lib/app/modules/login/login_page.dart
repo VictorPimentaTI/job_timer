5c00a70d2e2cd852b4544d252a90f3f0fd7dac3a
VictorPimentaTI <victorpimenta.ti@gmail.com>
2022-06-26 00:34:03 -0300

    Feito o visual do login page

--- lib/app/modules/login/login_page.dart
+++ lib/app/modules/login/login_page.dart
@@ -6,9 +6,39 @@ class LoginPage extends StatelessWidget {
 
    @override
    Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Login'),),
-           body: Container(),
-       );
+    final screenSize = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+            Color(0XFF0092B9),
+            Color(0XFF0167B2),
+            ],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/logo.png'),
+              SizedBox(height: screenSize.height * .1,),
+              SizedBox(
+                width: screenSize.width * .8,
+                height: screenSize.height * .08,
+                child: ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.grey[200]
+                  ),
+                  child: Image.asset('assets/images/google.png')
+                ),
+              )
+            ],
+          ),
+        ),
+      )
+    );
   }
 }
