bfe50d895f2accfbd52680dd51f50a8859a9699a
VictorPimentaTI <victorpimenta.ti@gmail.com>
2022-09-13 20:58:04 -0300

    Adicionado o controller dos campos e validator

--- lib/app/modules/project/register/project_register_page.dart
+++ lib/app/modules/project/register/project_register_page.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:validatorless/validatorless.dart';
 
-class ProjectRegisterPage extends StatelessWidget {
+class ProjectRegisterPage extends StatefulWidget {
 
   const ProjectRegisterPage({ Key? key }) : super(key: key);
 
+  @override
+  State<ProjectRegisterPage> createState() => _ProjectRegisterPageState();
+}
+
+class _ProjectRegisterPageState extends State<ProjectRegisterPage> {
+
+  final _formKey = GlobalKey<FormState>();
+  final _nameEC = TextEditingController();
+  final _estimateEC = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameEC.dispose();
+    _estimateEC.dispose();
+    super.dispose();
+  }
    @override
    Widget build(BuildContext context) {
        return Scaffold(
@@ -18,22 +35,31 @@ class ProjectRegisterPage extends StatelessWidget {
             elevation: 0,
            ),
            body: Form(
+            key: _formKey,
              child: Padding(
                padding: const EdgeInsets.all(8.0),
                child: Column(
                 children: [
                   TextFormField(
+                    controller: _nameEC,
                     decoration: const InputDecoration(
                       label: Text('Nome do projeto'),
                     ),
+                    validator: Validatorless.required('Nome obrigatório'),
                   ),
                   const SizedBox(
                      height: 10,
                   ),
                   TextFormField(
+                    controller: _estimateEC,
+                    keyboardType: TextInputType.number,
                     decoration: const InputDecoration(
                       label: Text('Estimativa de horas'),
                     ),
+                    validator: Validatorless.multiple([
+                      Validatorless.required('Estimativa obrigatória'),
+                      Validatorless.number('Só é permitido números')
+                    ]),
                   ),
                   const SizedBox(
                      height: 10,
@@ -42,7 +68,12 @@ class ProjectRegisterPage extends StatelessWidget {
                     width: MediaQuery.of(context).size.width,
                     height: 49,
                     child: ElevatedButton(
-                      onPressed: (){},
+                      onPressed: (){
+                        final formValid = _formKey.currentState?.validate() ?? false;
+                        if(formValid){
+                          
+                        }
+                      },
                       child: const Text('Salvar'),
                     ),
                   )
