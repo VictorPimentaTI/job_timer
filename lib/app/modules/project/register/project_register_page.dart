b53e646e3f9c6802d69788d0edf0479504b876ac
VictorPimentaTI <victorpimenta.ti@gmail.com>
2022-09-12 21:30:04 -0300

    Criando a marcação da tela

--- lib/app/modules/project/register/project_register_page.dart
+++ lib/app/modules/project/register/project_register_page.dart
@@ -7,8 +7,48 @@ class ProjectRegisterPage extends StatelessWidget {
    @override
    Widget build(BuildContext context) {
        return Scaffold(
-           appBar: AppBar(title: const Text('New Project'),),
-           body: Container(),
+           backgroundColor: Colors.white,
+           appBar: AppBar(
+             title: const Text(
+               'Criar novo projeto',
+               style: TextStyle(color: Colors.black)
+            ),
+            backgroundColor: Colors.white,
+            iconTheme: const IconThemeData(color: Colors.black),
+            elevation: 0,
+           ),
+           body: Form(
+             child: Padding(
+               padding: const EdgeInsets.all(8.0),
+               child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      label: Text('Nome do projeto'),
+                    ),
+                  ),
+                  const SizedBox(
+                     height: 10,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      label: Text('Estimativa de horas'),
+                    ),
+                  ),
+                  const SizedBox(
+                     height: 10,
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: 49,
+                    child: ElevatedButton(
+                      onPressed: (){},
+                      child: const Text('Salvar'),
+                    ),
+                  )
+                ],
+               ),
+             ),),
        );
   }
 }
